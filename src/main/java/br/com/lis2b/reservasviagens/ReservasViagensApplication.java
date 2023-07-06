60f0eae25a8ea0743832fb18085aa5f8679cc288
igoralves367 <igor.alves367@gmail.com>
2023-07-06 19:18:30 -0300

    init project

---
+++ src/main/java/br/com/lis2b/reservasviagens/ReservasViagensApplication.java
@@ -0,0 +1,13 @@
+package br.com.lis2b.reservasviagens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservasViagensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservasViagensApplication.class, args);
+	}
+
+}
