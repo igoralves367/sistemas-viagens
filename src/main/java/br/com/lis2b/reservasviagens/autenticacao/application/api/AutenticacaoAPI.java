8796b49ad8c45062e685e0bb21db4ae351bb6f7a
igoralves367 <igor.alves367@gmail.com>
2023-07-17 15:12:57 -0300

    Implementa camada de Autenticacao e Modifica Token e Credencial

---
+++ src/main/java/br/com/lis2b/reservasviagens/autenticacao/application/api/AutenticacaoAPI.java
@@ -0,0 +1,17 @@
+package br.com.lis2b.reservasviagens.autenticacao.application.api;
+
+import javax.validation.Valid;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/autenticacao")
+public interface AutenticacaoAPI {
+    @PostMapping
+    TokenResponse autentica(@RequestBody @Valid AutenticacaoRequest autenticacaoRequest) throws AuthenticationException;
+    @PostMapping("/reativacao")
+    TokenResponse reativaAutenticacao(@RequestHeader("Authorization") String tokenExpirado) throws AuthenticationException;
+}
