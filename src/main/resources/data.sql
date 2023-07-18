b21ff0818752bc3072dd7e3b5c96595b4718fe76
igoralves367 <igor.alves367@gmail.com>
2023-07-18 19:57:03 -0300

    configura app yml e retira data.sql

--- src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO Hospedagem (idHospedagem, localizacao, checkin, checkout, hotel, preco_total) 
-VALUES ('uuid1', 'Guarulhos São Paulo', '2023-08-01 00:00:00', '2023-08-05 00:00:00', 'North Place Hotel', 0.00);
